27653d27aa041cbb5a6e55cebb19db9a5a900dcf
DavidAMaldonadoH <64713060+DavidAMaldonadoH@users.noreply.github.com>
2022-02-18 20:38:30 -0600

    cambios p3

--- Practica_3/Abstract/Retorno.go
+++ Practica_3/Abstract/Retorno.go
@@ -1,19 +1,5 @@
 package abstract
 
-type Any interface {
-	String() string
-}
-
-type Entero struct {
-	Value int
-	Any
-}
-
-type Decimal struct {
-	Value float64
-	Any
-}
-
 type Retorno struct {
 	Value string
 	Tipo  Type
